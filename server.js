c05d03654756600b6e058c4fd6a40126dc8d94a9
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-10-13 20:48:24 -0400

    new notes function

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
+async function readFile() {
+  const data = await readFileAsync(__dirname + "/db/db.json", "UTF-8");
+  return JSON.parse(database);
+}
 // Getand listen to index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(dirname, "./public/index.html"));
@@ -20,8 +25,58 @@ app.get("/notes", (req, res) => {
 });
 
 // reads db file > returns saved notes as json
-app.post("/api/notes");
+app
+  .route("/api/notes")
+  .get(function (req, res) {
+    res.json(database);
+  })
 
+  // creates new note to the db json file
+  .post(function (req, res) {
+    let jsonFile = path.join(__dirname, "/db/db.json");
+    let newNote = req.body;
+    let maxNoteId = 100;
+
+    for (let i = 0; i < database.length; i++) {
+      let individualNote = database[i];
+
+      if (individualNote.id > highestNoteId) {
+        highestNoteId = individualNote.id;
+      }
+    }
+
+    newNote.id = highestNoteId + 1;
+    database.push(newNote);
+
+    fs.writeFile(jsonFile, JSON.stringify(database), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Note saved!");
+    });
+    res.json(newNote);
+  });
+
+// function to delete notes
+app.delete("/api/notes/:id", function (req, res) {
+  let jsonFile = path.join(__dirname, "/db/db.json");
+  // delete existing note by id
+  for (let i = 0; i < database.length; i++) {
+    if ((database[i].id = req.params.id)) {
+      // Remove 1 existing note via splice.
+      database.splice(i, 1);
+      break;
+    }
+  }
+  // Add updated notes back to the db.json file via WriteFile:
+  fs.writeFile(jsonFile, JSON.stringify(database), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your note has been deleted!");
+  });
+  res.json(database);
+});
 // starts server
 app.listen(PORT, () => {
   console.log("API server now on port 3000!" + PORT);
